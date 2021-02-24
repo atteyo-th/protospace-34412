5fdff48cd431b635ef0b59b9febf939ad370682f
atteyo-th <78345265+atteyo-th@users.noreply.github.com>
2021-02-24 12:45:44 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  def create
+    # binding.pry
+    @comment = Comment.new(comment_params)    
+    if @comment.save
+       redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
+    else
+       @prototype = @comment.prototype
+      #  @comment = @prototype.comment
+       render "prototypes/show" # views/prototypes/show.html.erbのファイルを参照
+    end
+  end
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
+
+
